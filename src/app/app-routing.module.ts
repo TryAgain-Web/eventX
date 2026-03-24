921b697bd4c106d766dab1d63491a4a37bd2e07d
TryAgain-Web <davinbowen09@gmail.com>
2026-03-24 15:06:23 -0500

    Login and sign up
    made a page for sign up and login. buttons aren't working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { EventsComponent } from './pages/events/events.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'signup', component: SignupComponent },
+  {path: 'events', component: EventsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { EventsComponent } from './pages/events/events.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent,
+    EventsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
